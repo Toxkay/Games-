42691671cb5a92cfe240df47ae979e79ca10656a
Shahd Ayman <162706752+shahdaymxn@users.noreply.github.com>
2024-12-15 21:29:26 +0200

    Update Main_Menu.cpp

--- Main_Menu.cpp
+++ Main_Menu.cpp
@@ -99,8 +99,62 @@ void pyramidTicTacToe() {
 }
 
 void fourInARow() {
-   
     cout << "You selected Four-in-a-row.\n";
+    
+    int n1 = 1, n2 = 2;
+    string name1, name2;
+
+    cout << "Enter Player " << n1 << " name: ";
+    getline(cin >> ws, name1);
+
+    cout << "Enter Player " << n2 << " name: ";
+    getline(cin >> ws, name2);
+
+    int type1 = 0;
+    cout << "Choose Player " << n1 << " type:\n1. Human\n2. Random Computer\n";
+    type1 = get_pos_integer("Enter your choice: ");
+    while (type1 != 1 && type1 != 2) {
+        cout << "Enter a valid choice (1 or 2): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        type1 = get_pos_integer("Enter your choice: ");
+    }
+
+    int type2 = 0;
+    cout << "Choose Player " << n2 << " type:\n1. Human\n2. Random Computer\n";
+    type2 = get_pos_integer("Enter your choice: ");
+    while (type2 != 1 && type2 != 2) {
+        cout << "Enter a valid choice (1 or 2): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        type2 = get_pos_integer("Enter your choice: ");
+    }
+
+    cout << "Player 1 " << name1 << " is assigned 'X'" << endl;
+    cout << "Player 2 " << name2 << " is assigned 'O'" << endl;
+    Player<char>* players[2];
+    ConnectFourBoard<char>* B = new ConnectFourBoard<char>();  
+
+    if (type1 == 1)
+        players[0] = new ConnectFourPlayer<char>(name1, 'X');
+    else if (type1 == 2)
+        players[0] = new RandomConnectFourPlayer<char>(name1, 'X');
+
+    if (type2 == 1)
+        players[1] = new ConnectFourPlayer<char>(name2, 'O');
+    else if (type2 == 2)
+        players[1] = new RandomConnectFourPlayer<char>(name2, 'O');
+
+    GameManager<char> G(B, players);  
+    G.run();  
+
+    delete B;
+    for (int i = 0; i < 2; ++i) {
+        delete players[i];
+    }
+
+    return 0;
+}
 }
 
 void fiveByFiveTicTacToe() {
@@ -228,6 +282,60 @@ void wordTicTacToe() {
 
 void numericalTicTacToe() {
     cout << "You selected Numerical Tic-Tac-Toe.\n";
+    int n1 = 1, n2 = 2;
+    string name1, name2;
+    cout << "Enter Player " << n1 << " name: ";
+    getline(cin >> ws, name1);
+
+    cout << "Enter Player " << n2 << " name: ";
+    getline(cin >> ws, name2);
+
+    
+    int type1 = 0;
+    cout << "Choose Player " << n1 << " type:\n1. Human\n2. Random Computer\n";
+    type1 = get_pos_integer("Enter your choice: ");
+    while (type1 != 1 && type1 != 2) {
+        cout << "Enter a valid choice (1 or 2): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        type1 = get_pos_integer("Enter your choice: ");
+    }
+
+    int type2 = 0;
+    cout << "Choose Player " << n2 << " type:\n1. Human\n2. Random Computer\n";
+    type2 = get_pos_integer("Enter your choice: ");
+    while (type2 != 1 && type2 != 2) {
+        cout << "Enter a valid choice (1 or 2): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        type2 = get_pos_integer("Enter your choice: ");
+    }
+
+    cout << "Player 1 " << name1 << " is assigned 'Odd' numbers (1, 3, 5, 7, 9)" << endl;
+    cout << "Player 2 " << name2 << " is assigned 'Even' numbers (2, 4, 6, 8)" << endl;
+
+    
+    NumericalTicTacToe<int>* game = new NumericalTicTacToe<int>();
+    if (type1 == 1)
+        game->set_player1(new HumanPlayer<int>(name1, 1));  
+    else if (type1 == 2)
+        game->set_player1(new RandomPlayer<int>(name1, 1)); 
+
+
+    if (type2 == 1)
+        game->set_player2(new HumanPlayer<int>(name2, 2));  
+    else if (type2 == 2)
+        game->set_player2(new RandomPlayer<int>(name2, 2)); 
+
+    game->play_game();
+
+    delete game->get_player1();
+    delete game->get_player2();
+    delete game;
+
+    return 0;
+}
+
 }
 
 void misereTicTacToe() {
