9d5540f6743265e420f007c98dda8abf486c0e9b
Shahd Ayman <162706752+shahdaymxn@users.noreply.github.com>
2024-12-17 00:53:29 +0200

    Add files via upload

--- Game_5.cpp
+++ Game_5.cpp
@@ -2,22 +2,16 @@
 #include <bits\stdc++.h>
 #include "Game_5.h"
 
+using namespace std;
+
 int main() {
-    cout << "Welcome to Numerical Tic-Tac-Toe!\n";
-    cout << "Player 1 uses odd numbers (1, 3, 5, 7, 9).\n";
-    cout << "Player 2 uses even numbers (2, 4, 6, 8).\n\n";
-
-
     NumericalBoard<int> board;
+    NumericalPlayer<int> player1("Player 1", 0, true);  
+    NumericalPlayer<int> player2("Player 2", 0, false);
 
-    NumericalPlayer<int> player1("Player 1", 1, true);  
-    NumericalPlayer<int> player2("Player 2", 2, false);
+    Player<int>* players[2] = {&player1, &player2};
 
-    Player<int>* players[] = {&player1, &player2};
     GameManager<int> gameManager(&board, players);
-
-    player1.setBoard(&board);
-    player2.setBoard(&board);
 
     gameManager.run();
 
