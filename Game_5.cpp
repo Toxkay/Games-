6f051b41f6538fd45bca454da9ff15ebe696a64e
Shahd Ayman <162706752+shahdaymxn@users.noreply.github.com>
2024-12-16 18:43:25 +0200

    Update Game_5.cpp

--- Game_5.cpp
+++ Game_5.cpp
@@ -7,9 +7,10 @@ int main() {
     cout << "Player 1 uses odd numbers (1, 3, 5, 7, 9).\n";
     cout << "Player 2 uses even numbers (2, 4, 6, 8).\n\n";
 
+
     NumericalBoard<int> board;
 
-    NumericalPlayer<int> player1("Player 1", 1, true);
+    NumericalPlayer<int> player1("Player 1", 1, true);  
     NumericalPlayer<int> player2("Player 2", 2, false);
 
     Player<int>* players[] = {&player1, &player2};
